28d6c72c552867e4246cf5ad35e08af9679f5d7e
Cameron Pulsford <cpulsfor@me.com>
2019-10-23 09:32:14 -0400

    Moves some funcs/pragmas around.

--- Sources/TextStyle/TextStyle.swift
+++ Sources/TextStyle/TextStyle.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 @dynamicMemberLookup
-public struct TextStyle: Equatable {
+public struct TextStyle {
 
     static let defaultParagraphStyle = NSMutableParagraphStyle()
 
@@ -16,25 +16,20 @@ public struct TextStyle: Equatable {
     public init(_ builder: (inout TextStyle) -> Void) {
         builder(&self)
     }
+}
 
-    public var attributes: [NSAttributedString.Key: Any] {
-        if let style = paragraphStyle {
-            var attributess = _attributes
-            attributess[.paragraphStyle] = style
-            return attributess
-        } else {
-            return _attributes
-        }
-    }
-    
+// MARK: Equatable
+
+extension TextStyle: Equatable {
+
     public static func == (lhs: TextStyle, rhs: TextStyle) -> Bool {
         return (lhs.attributes as NSDictionary).isEqual(to: rhs.attributes)
     }
 }
 
-// MARK: Attributes
-
 public extension TextStyle {
+
+    // MARK: Attributes
 
     @inlinable
     subscript<T>(key: NSAttributedString.Key) -> T? {
@@ -48,10 +43,160 @@ public extension TextStyle {
         }
     }
 
+    var attributes: [NSAttributedString.Key: Any] {
+        if let style = paragraphStyle {
+            var attributess = _attributes
+            attributess[.paragraphStyle] = style
+            return attributess
+        } else {
+            return _attributes
+        }
+    }
+
+    // MARK: Font
+
     var font: UIFont? {
         get { self[.font] }
         set { self[.font] = newValue }
     }
+
+    // MARK: Foreground color
+
+    var foregroundColor: UIColor? {
+        get { self[.foregroundColor] }
+        set { self[.foregroundColor] = newValue }
+    }
+
+    // MARK: Background color
+
+    var backgroundColor: UIColor? {
+        get { self[.backgroundColor] }
+        set { self[.backgroundColor] = newValue }
+    }
+
+    // MARK: Ligature
+
+    var ligature: Int? {
+        get { self[.ligature] }
+        set { self[.ligature] = newValue }
+    }
+
+    // MARK: Kern
+
+    var kern: CGFloat? {
+        get { self[.kern] }
+        set { self[.kern] = newValue }
+    }
+
+    // MARK: Strikethrough style
+
+    var strikethroughStyle: NSUnderlineStyle? {
+        get { self[.strikethroughStyle] }
+        set { self[.strikethroughStyle] = newValue }
+    }
+
+    // MARK: Strikethrough color
+
+    var strikethroughColor: UIColor? {
+        get { self[.strikethroughColor] }
+        set { self[.strikethroughColor] = newValue }
+    }
+
+    // MARK: Underline style
+
+    var underlineStyle: NSUnderlineStyle? {
+        get { self[.underlineStyle] }
+        set { self[.underlineStyle] = newValue }
+    }
+
+    // MARK: Underline color
+
+    var underlineColor: UIColor? {
+        get { self[.underlineColor] }
+        set { self[.underlineColor] = newValue }
+    }
+
+    // MARK: Stroke color
+
+    var strokeColor: UIColor? {
+        get { self[.strokeColor] }
+        set { self[.strokeColor] = newValue }
+    }
+
+    // MARK: Stroke width
+
+    var strokeWidth: CGFloat? {
+        get { self[.strokeWidth] }
+        set { self[.strokeWidth] = newValue }
+    }
+
+    // MARK: Shadow
+
+    var shadow: NSShadow? {
+        get { self[.shadow] }
+        set { self[.shadow] = newValue }
+    }
+
+    // MARK: Text effect
+
+    var textEffect: NSAttributedString.TextEffectStyle? {
+        get { self[.textEffect] }
+        set { self[.textEffect] = newValue }
+    }
+
+    // MARK: Attachment
+
+    var attachment: NSTextAttachment? {
+        get { self[.attachment] }
+        set { self[.attachment] = newValue }
+    }
+
+    // MARK: Link
+
+    var link: URL? {
+        get { self[.link] }
+        set { self[.link] = newValue }
+    }
+
+    // MARK: Baseline offset
+
+    var baselineOffset: CGFloat? {
+        get { self[.baselineOffset] }
+        set { self[.baselineOffset] = newValue }
+    }
+
+    // MARK: Obliqueness
+
+    var obliqueness: CGFloat? {
+        get { self[.obliqueness] }
+        set { self[.obliqueness] = newValue }
+    }
+
+    // MARK: Expansion
+
+    var expansion: CGFloat? {
+        get { self[.expansion] }
+        set { self[.expansion] = newValue }
+    }
+
+    // MARK: Writing direction
+
+    var writingDirection: [NSWritingDirection]? {
+        get { self[.writingDirection] }
+        set { self[.writingDirection] = newValue }
+    }
+
+    // MARK: Vertical glyph form
+
+    var verticalGlyphForm: Int? {
+        get { self[.verticalGlyphForm] }
+        set { self[.verticalGlyphForm] = newValue }
+    }
+}
+
+public extension TextStyle {
+
+    // MARK: Paragraph style
 
     /// The paragraph style. New values will be copied upon being set.
     /// - Note: Prefer to use the specific properties on `TextStyle` for manipulating the paragraph
@@ -60,106 +205,6 @@ public extension TextStyle {
         get { _paragraphStyle?.copy() as? NSParagraphStyle }
         set { _paragraphStyle = newValue?.mutableCopy() as? NSMutableParagraphStyle }
     }
-
-    var foregroundColor: UIColor? {
-        get { self[.foregroundColor] }
-        set { self[.foregroundColor] = newValue }
-    }
-
-    var backgroundColor: UIColor? {
-        get { self[.backgroundColor] }
-        set { self[.backgroundColor] = newValue }
-    }
-
-    var ligature: Int? {
-        get { self[.ligature] }
-        set { self[.ligature] = newValue }
-    }
-
-    var kern: CGFloat? {
-        get { self[.kern] }
-        set { self[.kern] = newValue }
-    }
-
-    var strikethroughStyle: NSUnderlineStyle? {
-        get { self[.strikethroughStyle] }
-        set { self[.strikethroughStyle] = newValue }
-    }
-
-    var strikethroughColor: UIColor? {
-        get { self[.strikethroughColor] }
-        set { self[.strikethroughColor] = newValue }
-    }
-
-    var underlineStyle: NSUnderlineStyle? {
-        get { self[.underlineStyle] }
-        set { self[.underlineStyle] = newValue }
-    }
-
-    var underlineColor: UIColor? {
-        get { self[.underlineColor] }
-        set { self[.underlineColor] = newValue }
-    }
-
-    var strokeColor: UIColor? {
-        get { self[.strokeColor] }
-        set { self[.strokeColor] = newValue }
-    }
-
-    var strokeWidth: CGFloat? {
-        get { self[.strokeWidth] }
-        set { self[.strokeWidth] = newValue }
-    }
-
-    var shadow: NSShadow? {
-        get { self[.shadow] }
-        set { self[.shadow] = newValue }
-    }
-
-    var textEffect: NSAttributedString.TextEffectStyle? {
-        get { self[.textEffect] }
-        set { self[.textEffect] = newValue }
-    }
-
-    var attachment: NSTextAttachment? {
-        get { self[.attachment] }
-        set { self[.attachment] = newValue }
-    }
-
-    var link: URL? {
-        get { self[.link] }
-        set { self[.link] = newValue }
-    }
-
-    var baselineOffset: CGFloat? {
-        get { self[.baselineOffset] }
-        set { self[.baselineOffset] = newValue }
-    }
-
-    var obliqueness: CGFloat? {
-        get { self[.obliqueness] }
-        set { self[.obliqueness] = newValue }
-    }
-
-    var expansion: CGFloat? {
-        get { self[.expansion] }
-        set { self[.expansion] = newValue }
-    }
-
-    var writingDirection: [NSWritingDirection]? {
-        get { self[.writingDirection] }
-        set { self[.writingDirection] = newValue }
-    }
-
-    var verticalGlyphForm: Int? {
-        get { self[.verticalGlyphForm] }
-        set { self[.verticalGlyphForm] = newValue }
-    }
-}
-
-// MARK: Paragraph style
-
-public extension TextStyle {
 
     subscript<T>(dynamicMember keyPath: WritableKeyPath<NSMutableParagraphStyle, T>) -> T {
         get {
